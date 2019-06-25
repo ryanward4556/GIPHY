73889b18a77e1d12459792ae1bc6b1ca714b8104
Ryan Ward <ryanward4556@gmail.com>
2019-06-25 13:57:18 -0400

    is able to place top 10 images on page after a button is clicked, new added buttons also function correctly. Need to add onclick event that will allow user to toggle each gif to play/be still

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,39 @@
 //  Global Variables
 
+var gifContainer = $("#gif-container")
 
 //  Sets up topics array of strings to be displayed at top of screen
 
 var topics = ["ajax", "barcelona", "real madrid", "psg", "atletico", "man city", "man united", "tottenham", "liverpool", "bayern"]
 
 
-//  Sets up queryURL
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=WBSORvGtsCMCwgWYyeKt6QjIwl0E7W8d";
 
 //  Creats button for each item in topics list
 
 // $("#topics-contatiner").html("<button>").text("button");
+
+
+function displayTopicInfo() {
+    //  Calls GIPHY api using queryURL
+    var search = $(this).attr("data-search-item");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=WBSORvGtsCMCwgWYyeKt6QjIwl0E7W8d";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            for(var i = 0; i < 10; i++) {
+                var imageURL = response.data[i].images.fixed_height.url;
+                var image = $("<img>").attr("src", imageURL);
+                gifContainer.append(image);
+            }
+            
+
+        });
+}
+
 
 // Function for displaying movie data
 function renderButtons() {
@@ -27,25 +49,36 @@ function renderButtons() {
         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var a = $("<button>");
         // Adding a class of movie to our button
-        a.addClass("team");
+        a.addClass("topic");
         // Adding a data-attribute
-        a.attr("data-team", topics[i]);
+        a.attr("data-search-item", topics[i]);
         // Providing the initial button text
         a.text(topics[i]);
         // Adding the button to the buttons-view div
         $("#topics-container").append(a);
     }
 }
+
+// This function handles events where one button is clicked
+$("#add-topic").on("click", function (event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    var topic = $("#topic-input").val().trim();
+
+    // Adding the movie from the textbox to our array
+    topics.push(topic);
+    console.log(topics);
+
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});
+
+
+$(document).on("click", ".topic", displayTopicInfo);
+
 renderButtons();
 
-//  Calls GIPHY api using queryURL
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-    .then(function (response) {
-        var results = response.data;
-        console.log(results);
-
-    });
+//  Create an on click event for the gif-stills. 
+//  Once clicked they're url should be replaced with the moving gif url
